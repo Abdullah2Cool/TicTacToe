2b34ce1b86a1cb57f5f9dd57b1d53807b41f155b
Abdullah Asad <abdullahbinasad14@gmail.com>
2019-02-09 22:13:31 -0500

    Draws a simple grid.

--- Prototype/main.js
+++ Prototype/main.js
@@ -1,6 +1,15 @@
 let app = new Vue({
     el: "#app",
     data: {
-        message: "Hello World"
+        Grid: [
+            ["top-left", "top-center", "top-right"],
+            ["center-left", "center-center", "center-right"],
+            ["bottom-left", "bottom-center", "bottom-right"]
+        ],
+        Status: [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
     }
 });
