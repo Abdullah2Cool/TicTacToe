c6c6120063d962c5d8cffddd65e0553df6a2a85e
Abdullah Asad <abdullahbinasad14@gmail.com>
2019-02-10 13:06:57 -0500

    Implemented a simple turn-based system.

--- Prototype/main.js
+++ Prototype/main.js
@@ -5,13 +5,14 @@ Vue.component('box', {
         }
     },
     computed: {
-
     },
     template: `
         <div class="col border p-2" v-on:click = "$emit('set-status', y, x)">
-            <i v-if="status === 0" class="fas fa-carrot fa-5x"></i>
-            <i v-else-if="status === 1" class="fas fa-cookie fa-5x"></i>
-            <i v-else class="far fa-question-circle fa-5x"></i>
+            <div>
+                <i v-if="status === 0" class="fas fa-carrot fa-3x p-5" style="color: crimson"></i>
+                <i v-else-if="status === 1" class="fas fa-cookie fa-3x p-5" style="color: blue"></i>
+                <i v-else class="far fa-question-circle fa-3x p-5" style="color: white"></i>
+            </div>
         </div>
     `
 })
@@ -24,23 +25,45 @@ let app = new Vue({
             ["center-left", "center-center", "center-right"],
             ["bottom-left", "bottom-center", "bottom-right"]
         ],
-        Status: [
+        Board: [
             -1, -1, -1,
             -1, -1, -1,
             -1, -1, -1
-        ]
+        ],
+        turn: 0,
+        p1Color: "crimson",
+        p2Color: "white"
     },
     methods: {
+        changeTurn() {
+            if (this.turn === 0) {
+                this.p1Color = "white";
+                this.p2Color = "blue";
+                this.turn = 1;
+            } else {
+                this.p1Color = "crimson";
+                this.p2Color = "white"
+                this.turn = 0;
+            }
+        },
         getStatus(y, x) {
             if (y > -1 && y < 3 && x > -1 && x < 3) {
-                return this.Status[y * 3 + x];
+                return this.Board[y * 3 + x];
             }
             return "error";
         },
         setStatus(y, x) {
             if (y > -1 && y < 3 && x > -1 && x < 3) {
                 let index = y * 3 + x;
-                this.$set(this.Status, index, this.Status[index] + 1);
+                if (this.Board[index] == -1) {
+                    this.$set(this.Board, index, this.turn);
+                    this.changeTurn();
+                }
+            }
+        },
+        resetGrid() {
+            for (let i = 0; i < 9; i++) {
+                this.$set(this.Board, i, -1);
             }
         }
     },
