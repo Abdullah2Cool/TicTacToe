2e2a9295ca483f4e81d0466657fd97f80c19ef6e
Abdullah Asad <abdullahbinasad14@gmail.com>
2019-02-10 00:58:30 -0500

    Modularized the Grid System. Potentially cumbersome approach.

--- Prototype/main.js
+++ Prototype/main.js
@@ -1,3 +1,19 @@
+Vue.component('box', {
+    props: ['y', 'x'],
+    data: () => {
+        return {
+            status: 2
+        }
+    },
+    template: `
+        <div class="col border p-2">
+            <i v-if="status === 0" class="fas fa-carrot fa-5x"></i>
+            <i v-else-if="status === 1" class="fas fa-cookie fa-5x"></i>
+            <i v-else class="far fa-question-circle fa-5x"></i>
+        </div>
+    `
+})
+
 let app = new Vue({
     el: "#app",
     data: {
@@ -8,8 +24,23 @@ let app = new Vue({
         ],
         Status: [
             [0, 0, 0],
-            [0, 0, 0],
+            [0, 1, 0],
             [0, 0, 0]
         ]
-    }
+    },
+    methods: {
+        getStatus(y, x) {
+            if (y > -1 && y < 3 && x > -1 && x < 3) {
+                return this.Status[y][x];
+            }
+            return "error";
+        },
+        setStatus(y, x) {
+            if (y > -1 && y < 3 && x > -1 && x < 3) {
+                this.Status[y][x] = 1;
+            }
+            console.log("HERE");
+        }
+    },
+
 });
